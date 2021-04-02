447532238533bda78ec13b6d8a584b41f94e4738
Ned <nedackland@gmail.com>
2021-04-02 14:03:18 +0800

    added pages for nav tab

--- src/components/ProfileBody/IGTV.vue
+++ src/components/ProfileBody/IGTV.vue
@@ -1,5 +1,11 @@
 <template>
-  <div v-if="isSelected">igtv tab</div>
+  <div class="igtv-wrapper" v-if="isSelected">
+    <div class="content">
+      <h2 class="content__header">Upload a Video</h2>
+      <p class="content__para">Videos must be between 1 and 60 minutes long</p>
+      <button>Upload</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,6 +17,37 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.igtv-wrapper {
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  margin: 100px 0;
+  .content {
+    max-width: 300px;
+    display: grid;
+    justify-content: center;
+    align-self: center;
+    text-align: center;
+    gap: 30px;
+    &__header {
+      font-size: xx-large;
+      font-weight: 500;
+    }
+    &__para {
+      font-size: small;
+    }
+  }
+  button{
+    cursor: pointer;
+    width: 30%;
+    height: 2em;
+    border-radius: 7px;
+    margin: 0 auto;
+    background-color: #0088ff;
+    border: 1px solid #0088ff;
+    color: white;
+    font-weight: bold;
+  }
+}
 </style>
--- src/components/ProfileBody/Saved.vue
+++ src/components/ProfileBody/Saved.vue
@@ -1,5 +1,12 @@
 <template>
-  <div v-if="isSelected">Saved</div>
+  <div v-if="isSelected">
+    <p class="info-text">Only you can see what you've saved</p>
+    <div class="posts-wrapper">
+      <div v-bind:key="post" v-for="post in savedPosts">
+        <div class="post"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,10 +14,37 @@ export default {
   name: "Saved",
   props: {
     isSelected: Boolean,
-  }
+  },
+  data() {
+    return {
+      savedPosts: [1, 2, 3, 4]
+    }
+  },
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.posts-wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  overflow-x: hidden;
+  max-width: 900px;
+  gap: 20px;
+  .post {
+    width: 100%;
+    height: 300px;
+    max-width: 350px;
+    max-height: 350px;
+    background-color: #e2e2e2;
+  }
+}
+.info-text {
+  text-align: center;
+  margin: 10px;
+  color: #8d8d8d;
+  font-size: small;
+}
 </style>
